63959e68474a9d06810a9ff1793ad6fcb45e4ad6
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2017-10-22 13:10:16 +0200

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,12 @@
+public class Board {
+    private Field[] fields;
+
+    public Board(){
+        fields = new Field[40];
+        for(int i = 0; i < 40; i++){
+            fields[i] = new Field("Field" + (i + 1), (i + 1));
+            System.out.println(fields[i]);
+        }
+    }
+
+}
