450a343c926c361d0602146e494b7be2c511fbbc
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2017-10-23 15:54:22 +0200

    Added two classes

---
+++ MonopolyConstants.java
@@ -0,0 +1,17 @@
+public class MonopolyConstants {
+    int START_MONEY = 1500;
+    int JAIL_POS = 11;
+    int PASSING_START = 200;
+    int INDKOMSTSKAT = 200;
+    int STATSSKAT = 100;
+
+    String[] FIELD_NAMES =
+            {
+                    "Start", "Rødovrevej", "Prøv lykken", "Hvidovrevej", "Indkomstskat", "Øresund A/S",
+                    "Roskildevej", "?", "Valby Langgade", "Allégade", "Fængsel", "Frederiksberg Allé", "Tuborg",
+                    "Büllowsvej", "Gl. Kongevej", "D.F.D.S A/S", "Bernstorffsvej", "?", "Hellerupvej", "Strandvejen",
+                    "Parkering", "Trianglen", "?", "Østerbrogade", "Grønningen", "Ø.K. A/S", "Bredgade",
+                    "Kgs Nytorv", "Carlsberg", "Østergade", "Gå i Fængsel!", "Amagertorv", "Vimmelskaftet", "?", "Nygade",
+                    "D/S Bornholm 1866", "?", "Frederiksberg Allé", "Statsskat", "Rådhuspladsen"
+            };
+}
